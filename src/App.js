a5dcb2b97ea9394b09a520e66591ed76ae34c635
Asaf Blum <asaf@honeybook.com>
2020-03-15 23:38:49 +0200

    downgrade scripts

--- src/App.js
+++ src/App.js
@@ -1,66 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useEffect, useState} from 'react';
+
+import Square from './Square.js'
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectors, actions } from './features/counter';
+
+// import { selectors, actions } from './features/counter';
+import update from 'immutability-helper'
+import { DndProvider } from 'react-dnd'
+import Backend from 'react-dnd-html5-backend'
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+const orderedColors = [
+    {
+        id: 1,
+        color:'#9400D3',
+    },
+    {
+        id: 2,
+        color: '#4B0082',
+    },
+    {
+        id: 3,
+        color: '#0000FF',
+    },
+    {
+        id: 4,
+        color: '#00FF00',
+    },
+    {
+        id: 5,
+        color: '#FFFF00'
+    },
+    {
+        id: 6,
+        color: '#FF7F00',
+    },
+    {
+        id: 7,
+        color: '#FF0000',
+    },
+]
+
+function isSorted(arr){
+    let sum = 0;
+    arr.slice(1).forEach((item, index) => {sum+= item- arr[index]})
+ return Math.abs(sum) === (arr.length - 1)
+
+
+}
 
 function App() {
-  const count = useSelector(selectors.selectCount);
-  const dispatch = useDispatch();
+    const [colors, setColors] = useState(shuffle(orderedColors))
+    const [didWin, setDidWin] = useState(false)
+
+
+    const moveCard = useCallback(
+        (dragIndex, hoverIndex) => {
+            const dragCard = colors[dragIndex]
+            setColors(
+                update(colors, {
+                    $splice: [
+                        [dragIndex, 1],
+                        [hoverIndex, 0, dragCard],
+                    ],
+                }),
+            )
+
+        },
+        [colors],
+    )
+
+    useEffect(() => {
+
+        const arr = colors.map((item) => item.id)
+        if (isSorted(arr)){
+            setDidWin(true);
+        }
+
+    },[colors])
+
+    const renderSquare = (square, index) => {
+        return (
+            <Square
+                key={square.id}
+                index={index}
+                id={square.id}
+                color={square.color}
+                moveCard={moveCard}
+            />
+        )
+    }
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-counter">
-          <button
-            className="App-counter-button"
-            aria-label="Increment value"
-            onClick={() => dispatch(actions.increment())}
-          >
-            +
-          </button>
-          <span className="App-counter-value">{count}</span>
-          <button
-            className="App-counter-button"
-            aria-label="Decrement value"
-            onClick={() => dispatch(actions.decrement())}
-          >
-            -
-          </button>
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          Learn
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>,
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>,
-          and
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+
+        {didWin && <h1>WOWOWOWOWOWOWOOWWO!!!!</h1>}
+
+        {/*<button onClick={()=> setColors(shuffle(orderedColors))}></button>*/}
+
+
+        <DndProvider backend={Backend}>
+
+
+      <div className="Colors">
+
+
+          <>
+              {colors.map((color, i) => renderSquare(color, i))}
+          </>
+
+
+      </div>
+        </DndProvider>
     </div>
   );
 }
